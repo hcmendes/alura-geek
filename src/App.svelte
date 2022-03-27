86aeaa36e352c9f82bce786ee67d03233bc24c1f
Hilton M. Cardoso <hilton.m.cardoso@gmail.com>
2022-03-27 13:58:05 -0300

    commit inicial

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<header class="header">
+		<img src="images/logo.svg" alt="logo" />
+		AluraGeek
+		<button>Login</button>
+		<img src="images/busca.svg" alt="busca" />
+	</header>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
