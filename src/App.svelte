f616e8afa677e6379bab08d08ef6c88af9718682
Hilton M. Cardoso <hilton.m.cardoso@gmail.com>
2022-04-02 15:52:40 -0300

    estilização do cabeçalho

--- src/App.svelte
+++ src/App.svelte
@@ -4,24 +4,53 @@
 
 <main>
 	<header class="header">
-		<img src="images/logo.svg" alt="logo" />
-		AluraGeek
-		<button>Login</button>
+		<div class="logo">
+			<h1>
+				<a href="_blank">
+					<img src="images/logo.svg" alt="logo" />
+				</a>
+			</h1>
+		</div>
+		<a class="login-button" href="#">Login</a>
 		<img src="images/busca.svg" alt="busca" />
 	</header>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	header {
+		display: flex;
+    justify-content: space-between;
+		align-items: center;
+    padding: 16px;
+		gap: 2rem;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.logo h1 {
+		margin: 0;
+		font-size: 1.5em;
+    width: 100px;
+	}
+
+	.logo a {
+		display: flex;
+	}
+
+	.logo img {
+		width: 100%;
+	}
+
+	.login-button {
+		padding: 0.75rem 1rem;
+    flex-grow: 1;
+    border: 1px solid #2A7AE4;
+    background-color: transparent;
+		margin-bottom: 0;
+		color: #2A7AE4;
+		font-family: 'Raleway', sans-serif;
+		font-weight: 600;
+		font-size: 0.875rem;
+		line-height: 1rem;
+		font-style: normal;
+		text-align: center;
 	}
 </style>
